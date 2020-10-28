b67e3182932281865c83f8a5e53bb718f75d16e5
Sebastian Mocanu <diemetal851@gmail.com>
2020-10-28 18:12:16 +0200

    add user

--- app.js
+++ app.js
@@ -16,5 +16,56 @@ async function getRandomUser(){
   const res = await fetch("https://randomuser.me/api");
   const data = await res.json();
 
-  console.log(data);
+  const user = data.results[0];
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000)
+  };
+
+  // console.log(newUser);
+  addData(newUser);
 }
+
+// Add new obj to data Array
+
+function addData(obj) {
+  data.push(obj);
+  updateDOM();
+}
+
+// Update DOM
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+  providedData.forEach(item => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+    main.appendChild(element);
+  });
+
+}
+
+// Format number as money
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event listeners
+addUserBtn.addEventListener("click", getRandomUser);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
