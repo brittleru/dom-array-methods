fc4608ccbfea80877c585a9bccb3b35b0637fbad
Sebastian Mocanu <diemetal851@gmail.com>
2020-10-28 19:31:11 +0200

    final

--- app.js
+++ app.js
@@ -47,6 +47,13 @@ function showMillionaires() {
   updateDOM();
 }
 
+// Calculate total wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
 
 // Add new obj to data Array
 
@@ -78,16 +85,4 @@ addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
 showMillionairesBtn.addEventListener("click", showMillionaires);
-
-
-
-
-
-
-
-
-
-
-
-
-//
+calculateWealthBtn.addEventListener("click", calculateWealth);
