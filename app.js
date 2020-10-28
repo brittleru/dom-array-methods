49a3fd537996cd0f9cb17a0c87cbb311d5d89267
Sebastian Mocanu <diemetal851@gmail.com>
2020-10-28 19:11:54 +0200

    sort by richest

--- app.js
+++ app.js
@@ -33,7 +33,12 @@ function doubleMoney() {
   });
 
   updateDOM();
+}
 
+// Sort users by richest
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money);
+  updateDOM();
 }
 
 
@@ -65,7 +70,7 @@ function formatMoney(number) {
 // Event listeners
 addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
-
+sortBtn.addEventListener("click", sortByRichest);
 
 
 
